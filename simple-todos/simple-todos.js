3cf7b7c23d7e9d06816c361c0cf2d34bd4e58a81
Jason <bjason@gmail.com>
2014-11-29 18:42:52 -0800

    todos

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -18,6 +18,25 @@ if (Meteor.isClient) {
  tasks: function () {
       return Tasks.find({});
     }
+        ,
+
+  "submit .new-task": function (event) {
+    // This function is called when the new task form is submitted
+
+    var text = event.target.text.value;
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.text.value = "";
+
+    // Prevent default form submit
+    return false;
+  }
+
 
   });
 
