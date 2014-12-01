55320b7ba40336982e31b07cbd9b2465d10db098
Jason <bjason@gmail.com>
2014-11-30 23:27:18 -0800

    sort tasks

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -16,11 +16,18 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
  tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: 1});
     }
-        ,
+        
 
+
+  });
+
+
+
+Template.body.events({
   "submit .new-task": function (event) {
+    debugger;
     // This function is called when the new task form is submitted
 
     var text = event.target.text.value;
@@ -38,13 +45,15 @@ if (Meteor.isClient) {
   }
 
 
-  });
-
+});
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
     }
+
+
+
   });
 }
 
